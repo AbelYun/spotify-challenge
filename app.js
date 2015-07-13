4ed8b34682c2ff396d9bb6db60910e3cb2977e80
unknown <lz8702@WACDTL55LZ8702.ITServices.sbc.com>
2015-07-12 20:56:27 -0700

    update

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
   $scope.getSongs = function() {
     $http.get(baseUrl + $scope.track).success(function(response){
+
       data = $scope.tracks = response.tracks.items
       
     })
   }
+
+  $scope.master = {};
+  $scope.user = {};
+  $scope.minLength = 2;
+  $scope.submit = function() {
+    alert('Name: ' + $scope.user.name + '   Email:' + $scope.user.email);
+    $scope.user = angular.copy($scope.master);
+  };
+
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
@@ -24,6 +34,7 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     }
   }
 })
+
 
 // Add tool tips to anything with a title property
 $('body').tooltip({
